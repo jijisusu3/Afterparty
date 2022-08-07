8f6c871b266f723a424cea48663c38bee129f965
kahyu <kahyu1597@gmail.com>
2022-08-07 11:11:07 +0900

    Feat : 회원가입 중복 검사를 위한 api 추가 등록

--- frontend/src/api/secosi.js
+++ frontend/src/api/secosi.js
@@ -6,5 +6,8 @@ export default {
     login: () => HOST + "auth/login",
     signup: () => HOST + ACCOUNTS ,
     currentUserInfo: () => HOST + ACCOUNTS + '/profile',
+    userIdCheck: () => HOST + ACCOUNTS + '/check-userid',
+    emailCheck: () => HOST + ACCOUNTS + '/check-email',
+    nicknameCheck: () => HOST + ACCOUNTS + '/check-name',
   }
 }
