2cb505f9355377962cf26c7f4faa1e127d61a807
chodayeon <dus6982@naver.com>
2022-08-10 16:37:06 +0900

    Feat : 화상회의방 생성 기능 추가

--- backend/src/main/java/com/ssafy/api/service/ConferenceService.java
+++ backend/src/main/java/com/ssafy/api/service/ConferenceService.java
@@ -2,11 +2,10 @@ package com.ssafy.api.service;
 
 
 import com.ssafy.api.request.ConferenceGetReq;
-import com.ssafy.api.request.PerformGetReq;
+import com.ssafy.api.request.ConferenceRegistPostReq;
 import com.ssafy.api.response.ConferenceRes;
-import com.ssafy.api.response.PerformInfoRes;
-import com.ssafy.api.response.PerformRes;
-import com.ssafy.db.entity.Gugun;
+import com.ssafy.db.entity.Conference;
+import com.ssafy.db.entity.User;
 
 import java.util.List;
 
@@ -23,4 +22,6 @@ public interface ConferenceService {
     List<ConferenceRes> searchConference(ConferenceGetReq searchInfo);
 
     List<ConferenceRes> searchConferenceNoKeyword(ConferenceGetReq searchInfo);
+
+    Conference createConference(User user, ConferenceRegistPostReq conferenceInfo);
 }
