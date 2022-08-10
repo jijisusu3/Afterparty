c455c4617a3318aadbb818a971887a3c2867a4db
chodayeon <dus6982@naver.com>
2022-08-11 00:22:12 +0900

    Style : Request 코드 통일

--- backend/src/main/java/com/ssafy/api/request/UserRegisterPostReq.java
+++ backend/src/main/java/com/ssafy/api/request/UserRegisterPostReq.java
@@ -6,9 +6,10 @@ import lombok.Getter;
 import lombok.Setter;
 
 /**
- * 유저 회원가입 API ([POST] /api/v1/users) 요청에 필요한 리퀘스트 바디 정의.
+ * 유저 회원가입 API ([POST] /api/users) 요청에 필요한 리퀘스트 바디 정의.
  * UserRegisterRequestDto
  */
+
 @Getter
 @Setter
 @ApiModel("UserRegisterPostRequest")
@@ -16,10 +17,13 @@ public class UserRegisterPostReq {
 	//입력해서 넘기는 request body = id, nickname, email, password
 	@ApiModelProperty(name="유저 ID", example="ssafy_web")
 	String userId;
+
 	@ApiModelProperty(name="유저 Name", example="kim_ssafy")
 	String name;
+
 	@ApiModelProperty(name="유저 Email", example="ssafy_web@ssafy.com")
 	String email;
+
 	@ApiModelProperty(name="유저 Password", example="your_password")
 	String password;
 }
