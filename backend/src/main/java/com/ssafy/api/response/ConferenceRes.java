372e11c35d56af997463372d4f2b1d3345d0d8c3
차송희 <chasonghui@naver.com>
2022-08-10 21:41:34 +0900

    Revert "Merge branch 'be-article' into 'develop'"
    This reverts merge request !62

--- backend/src/main/java/com/ssafy/api/response/ConferenceRes.java
+++ backend/src/main/java/com/ssafy/api/response/ConferenceRes.java
@@ -18,22 +18,14 @@ public class ConferenceRes {
     //스포금지/주의 여부
     boolean is_spoiler;
     //시작 시간
-//    LocalDateTime call_start_time;
-    // 공연 일시
-    String perform_day;
-    //방장ID
-    String owner_id;
-
+    private LocalDateTime call_start_time;
     public static ConferenceRes of(Conference conference){
         ConferenceRes res = new ConferenceRes();
 
         res.setTitle(conference.getTitle());
         res.setPrfnm(conference.getPrfnm());
         res.set_spoiler(conference.is_spoiler());
-//        res.setCall_start_time(conference.getCall_start_time());
-        res.setPerform_day(conference.getPerform_day());
-        res.setOwner_id(conference.getOwner_id());
-
+        res.setCall_start_time(conference.getCall_start_time());
         return res;
     }
 }
