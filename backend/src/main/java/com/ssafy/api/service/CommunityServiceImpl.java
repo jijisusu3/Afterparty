16269a7f105aeaf171ecaabc230d29406cf9ac95
차송희 <chasonghui@naver.com>
2022-08-10 21:36:18 +0900

    Fix : 게시글 조회시 게시글 번호 컬럼 추가

--- backend/src/main/java/com/ssafy/api/service/CommunityServiceImpl.java
+++ backend/src/main/java/com/ssafy/api/service/CommunityServiceImpl.java
@@ -2,7 +2,6 @@ package com.ssafy.api.service;
 
 import com.ssafy.api.request.CommunityRegistPostReq;
 import com.ssafy.api.response.CommunityRes;
-import com.ssafy.api.response.FollowingRes;
 import com.ssafy.db.entity.Comment;
 import com.ssafy.db.entity.Community;
 import com.ssafy.db.entity.User;
@@ -16,8 +15,6 @@ import org.springframework.stereotype.Service;
 import javax.transaction.Transactional;
 import java.util.ArrayList;
 import java.util.List;
-
-import static com.ssafy.db.entity.QCommunity.community;
 
 /**
  *	커뮤니티 API 관련 비즈니스 로직 처리를 위한 서비스 구현 정의.
@@ -54,10 +51,7 @@ public class CommunityServiceImpl implements CommunityService{
         List<Community> communityList = communityRepositorySupport.findCommunityListByGenre(genre, category);
         List<CommunityRes> res = new ArrayList<>();
         for(Community commu : communityList){
-            res.add(CommunityRes.of(commu.getArticle_title(),
-                    commu.getUser().getUserId(),
-                    commu.getView_cnt(),
-                    commu.getRecommend(),
+            res.add(CommunityRes.of(commu,
                     commentRepositorySupport.countCommentByArticleId(commu.getArticle_id())));
         }
         return res;
@@ -68,10 +62,7 @@ public class CommunityServiceImpl implements CommunityService{
         List<Community> communityList = communityRepositorySupport.findCommunityListSearch(genre, category, searchcategory, searchword);
         List<CommunityRes> res = new ArrayList<>();
         for(Community commu : communityList){
-            res.add(CommunityRes.of(commu.getArticle_title(),
-                    commu.getUser().getUserId(),
-                    commu.getView_cnt(),
-                    commu.getRecommend(),
+            res.add(CommunityRes.of(commu,
                     commentRepositorySupport.countCommentByArticleId(commu.getArticle_id())));
         }
         return res;
@@ -84,11 +75,7 @@ public class CommunityServiceImpl implements CommunityService{
         List<Community> communityList = communityRepositorySupport.findAllCommunityList();
         List<CommunityRes> res = new ArrayList<>();
         for(Community commu: communityList){
-            res.add(CommunityRes.of(
-                    commu.getArticle_title(),
-                    commu.getUser().getUserId(),
-                    commu.getView_cnt(),
-                    commu.getRecommend(),
+            res.add(CommunityRes.of(commu,
                     commentRepositorySupport.countCommentByArticleId(commu.getArticle_id())));
         }
         return res;
@@ -158,11 +145,7 @@ public class CommunityServiceImpl implements CommunityService{
         List<Community> communityList = communityRepositorySupport.findCommunityListByUserId(userId);
         List<CommunityRes> res = new ArrayList<>();
         for(Community commu: communityList){
-            res.add(CommunityRes.of(
-                    commu.getArticle_title(),
-                    commu.getUser().getUserId(),
-                    commu.getView_cnt(),
-                    commu.getRecommend(),
+            res.add(CommunityRes.of(commu,
                     commentRepositorySupport.countCommentByArticleId(commu.getArticle_id())));
         }
         return res;
@@ -183,11 +166,7 @@ public class CommunityServiceImpl implements CommunityService{
         List<Community> communityList = communityRepositorySupport.findArticleByRecommend();
         List<CommunityRes> res = new ArrayList<>();
         for(Community commu: communityList){
-            res.add(CommunityRes.of(
-                    commu.getArticle_title(),
-                    commu.getUser().getUserId(),
-                    commu.getView_cnt(),
-                    commu.getRecommend(),
+            res.add(CommunityRes.of(commu,
                     commentRepositorySupport.countCommentByArticleId(commu.getArticle_id())));
         }
         return res;
