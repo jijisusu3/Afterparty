4fa840f4e60b7d0a88fbace632e36e6ca062105a
차송희 <chasonghui@naver.com>
2022-08-19 09:49:20 +0900

    Revert "Merge branch 'backend' into 'develop'"
    This reverts merge request !166

--- frontend/src/api/secosi.js
+++ frontend/src/api/secosi.js
@@ -1,5 +1,5 @@
-// const HOST = "http://localhost:8080/api/"
-const HOST = "https://i7b303.p.ssafy.io/api/"
+const HOST = "http://localhost:8080/api/"
+// const HOST = "https://i7b303.p.ssafy.io/api/"
 const ACCOUNTS = "users"
 const COMMUNITIES = "communities"
 const PERFORMS = 'performs'
